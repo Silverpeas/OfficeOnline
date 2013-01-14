581fa5ffe2f8428c4b3e1e7536f427bbefb1f4c8
Emmanuel Hugonnet <emmanuel.hugonnet@silverpeas.com>
2013-01-14 16:01:44 +0100

    Fxing bug #4143 and changing the parameters order

--- src/main/java/org/silverpeas/openoffice/Launcher.java
+++ src/main/java/org/silverpeas/openoffice/Launcher.java
@@ -1,27 +1,23 @@
 /**
  * Copyright (C) 2000 - 2009 Silverpeas
  *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Affero General Public License as
- * published by the Free Software Foundation, either version 3 of the
- * License, or (at your option) any later version.
+ * This program is free software: you can redistribute it and/or modify it under the terms of the
+ * GNU Affero General Public License as published by the Free Software Foundation, either version 3
+ * of the License, or (at your option) any later version.
  *
- * As a special exception to the terms and conditions of version 3.0 of
- * the GPL, you may redistribute this Program in connection with Free/Libre
- * Open Source Software ("FLOSS") applications as described in Silverpeas's
- * FLOSS exception.  You should have received a copy of the text describing
- * the FLOSS exception, and it is also available here:
+ * As a special exception to the terms and conditions of version 3.0 of the GPL, you may
+ * redistribute this Program in connection with Free/Libre Open Source Software ("FLOSS")
+ * applications as described in Silverpeas's FLOSS exception. You should have received a copy of the
+ * text describing the FLOSS exception, and it is also available here:
  * "http://repository.silverpeas.com/legal/licensing"
  *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Affero General Public License for more details.
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Affero General Public License for more details.
  *
- * You should have received a copy of the GNU Affero General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * You should have received a copy of the GNU Affero General Public License along with this program.
+ * If not, see <http://www.gnu.org/licenses/>.
  */
-
 package org.silverpeas.openoffice;
 
 import org.silverpeas.openoffice.util.MessageDisplayer;
@@ -48,36 +44,36 @@ public class Launcher {
   static final String LAUNCHER_VERSION = "2.12";
   static final MimetypesFileTypeMap mimeTypes = new MimetypesFileTypeMap();
   static final Logger logger = Logger.getLogger(Launcher.class.getName());
-  
+
   /**
    * @param args the command line arguments
-   * @throws OfficeNotFoundException  
+   * @throws OfficeNotFoundException
    */
   public static void main(final String[] args) throws OfficeNotFoundException {
-    logger.log(Level.INFO, "{0} version {1}", new Object[] { MessageUtil.getMessage("app.title"),
-        LAUNCHER_VERSION });
-    logger.log(Level.INFO, "{0}{1}", new Object[] { MessageUtil.getMessage("info.url.encoded"),
-        args[0] });
+    logger.log(Level.INFO, "{0} version {1}", new Object[]{MessageUtil.getMessage("app.title"),
+      LAUNCHER_VERSION});
+    logger.log(Level.INFO, "{0}{1}", new Object[]{MessageUtil.getMessage("info.url.encoded"),
+      args[0]});
     try {
       String url = UrlExtractor.extractUrl(args[0]);
-      logger.log(Level.INFO, "{0}{1}", new Object[] { MessageUtil.getMessage("info.url.decoded"),
-          url });
+      logger.log(Level.INFO, "{0}{1}", new Object[]{MessageUtil.getMessage("info.url.decoded"),
+        url});
       if (args[1] != null && !"".equals(args[1].trim())) {
-        logger.log(Level.INFO, "{0} {1}", new Object[] {
-            MessageUtil.getMessage("info.default.path"), UrlExtractor.decodePath(args[1]) });
+        logger.log(Level.INFO, "{0} {1}", new Object[]{
+          MessageUtil.getMessage("info.default.path"), UrlExtractor.decodePath(args[1])});
         MsOfficePathFinder.basePath = UrlExtractor.decodePath(args[1]);
       }
-      AuthenticationInfo authInfo = null;
-      if (args.length >= 4) {
-        authInfo = PasswordManager.extractAuthenticationInfo(args[2], args[3]);
-      }
       boolean useDeconnectedMode = false;
-      if(args.length >= 5) {
-        useDeconnectedMode = getBooleanValue(args[4]);
-      } 
+      if (args.length >= 3) {
+        useDeconnectedMode = getBooleanValue(args[2]);
+      }
+      AuthenticationInfo authInfo = null;
+      if (args.length >= 5) {
+        authInfo = PasswordManager.extractAuthenticationInfo(args[3], args[4]);
+      }
       MsOfficeType contentType = getContentType(UrlExtractor.decodeUrl(args[0]));
-      logger.log(Level.FINE, "{0}{1}", new Object[] { MessageUtil.getMessage("info.document.type"),
-          contentType });
+      logger.log(Level.FINE, "{0}{1}", new Object[]{MessageUtil.getMessage("info.document.type"),
+        contentType});
       defineLookAndFeel();
       System.exit(OfficeLauncher.launch(contentType, url, authInfo, useDeconnectedMode));
     } catch (IOException ex) {
@@ -95,11 +91,11 @@ public class Launcher {
       System.exit(0);
     }
   }
-  
+
   protected static boolean getBooleanValue(final String expression) {
     return "true".equalsIgnoreCase(expression) || "yes".equalsIgnoreCase(expression)
-      || "y".equalsIgnoreCase(expression) || "oui".equalsIgnoreCase(expression)
-      || "1".equalsIgnoreCase(expression);
+        || "y".equalsIgnoreCase(expression) || "oui".equalsIgnoreCase(expression)
+        || "1".equalsIgnoreCase(expression);
   }
 
   protected static void defineLookAndFeel() {
